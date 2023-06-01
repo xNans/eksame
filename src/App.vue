6f0c0a63fa3fb91a5f6bf7e96200f487a4acfe74
xnans <nannamc@live.dk>
2023-06-01 20:24:24 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,31 @@
 </template>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  font-size: 30px;
 }
 
 nav {
   padding: 30px;
+  background-color: #204B82;
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: white;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #F5A22C;
 }
 </style>
