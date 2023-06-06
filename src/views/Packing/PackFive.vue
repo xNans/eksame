d90ef898ed56f5968ba742381556790645f54325
xnans <nannamc@live.dk>
2023-06-06 03:11:22 +0200

    yaay

--- src/views/Packing/PackFive.vue
+++ src/views/Packing/PackFive.vue
@@ -4,7 +4,7 @@
         <h1>Påsætning af kantlister</h1>
         <div class="player-container">
             <video width="1080" height="720" controls>
-                <source src="../../assets/packaging" type="video/mp4">
+                <source src="../../assets/packaging/kantlister.mp4" type="video/mp4">
                 Your browser does not support the video tag.
             </video> 
         </div>
